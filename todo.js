edcdc04ea30ec1319af9561d3ecada0a1f568937
myChap01 <khh_123@naver.com>
2020-05-08 11:57:24 +0900

    fourth commit

--- todo.js
+++ todo.js
@@ -4,15 +4,41 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos
+    saveToDos();
+}
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     delBtn.innerText = "X";
+    delBtn.addEventListener("click", deleteToDo);
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
     span.innerText = text;
     li.appendChild(span);
     li.appendChild(delBtn);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event) {
@@ -23,8 +49,12 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if (toDos !== null) {
+    const loadedTodos = localStorage.getItem(TODOS_LS);
+    if (loadedTodos !== null) {
+        const parsedToDos = JSON.parse(loadedTodos);
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        });
 
     }
 }
